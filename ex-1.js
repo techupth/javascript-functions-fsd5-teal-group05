92c64003331dacd447d3e9c831a5add6cda6af9b
Dennis <suwatchai.kaew@gmail.com>
2023-11-29 13:10:46 +0700

    fix: edit variable name

--- ex-1.js
+++ ex-1.js
@@ -1,33 +1,28 @@
 // Start coding here
-function add(a,b)
-{
-    let result = a+b
-    return result;
+function add(a, b) {
+  let result = a + b;
+  return result;
 }
-let test1 = add(5,10)
-console.log(test1)
+let test1 = add(5, 10);
+console.log(test1);
 
-
-function subtarct(a,b)
-{
-    let result = a-b
-    return result;
+function subtarct(a, b) {
+  let result = a - b;
+  return result;
 }
-let test2 = subtarct(10,5)
-console.log(test2)
+let test2 = subtract(10, 5);
+console.log(test2);
 
-function multiply(a,b)
-{
-    let result = a*b
-    return result;
+function multiply(a, b) {
+  let result = a * b;
+  return result;
 }
-let test3 = multiply(10,5)
-console.log(test3)
+let test3 = multiply(10, 5);
+console.log(test3);
 
-function divide(a,b)
-{
-    let result = a/b
-    return result;
+function divide(a, b) {
+  let result = a / b;
+  return result;
 }
-let test4 = divide(10,5)
-console.log(test4)
+let test4 = divide(10, 5);
+console.log(test4);
--- ex-2.js
+++ ex-2.js
@@ -1,33 +1,28 @@
 // Start coding here
-let add = function (a,b)
-{
-    let result = a+b
-    return result;
-}
-let test1 = add(5,10)
-console.log(test1)
+let add = function (a, b) {
+  let result = a + b;
+  return result;
+};
+let test1 = add(5, 10);
+console.log(test1);
 
+let subtract = function (a, b) {
+  let result = a - b;
+  return result;
+};
+let test2 = subtract(10, 5);
+console.log(test2);
 
-let subtarct = function (a,b)
-{
-    let result = a-b
-    return result;
-}
-let test2 = subtarct(10,5)
-console.log(test2)
+let multiply = function (a, b) {
+  let result = a * b;
+  return result;
+};
+let test3 = multiply(10, 5);
+console.log(test3);
 
-let  multiply =function(a,b)
-{
-    let result = a*b
-    return result;
-}
-let test3 = multiply(10,5)
-console.log(test3)
-
-let divide = function (a,b)
-{
-    let result = a/b
-    return result;
-}
-let test4 = divide(10,5)
-console.log(test4)
+let divide = function (a, b) {
+  let result = a / b;
+  return result;
+};
+let test4 = divide(10, 5);
+console.log(test4);
